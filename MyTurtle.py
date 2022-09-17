6ae03dcb03d6cfca235cf8967557f4cfe2cf4975
CL76 <83503901+CL76@users.noreply.github.com>
2022-09-17 15:38:35 +0200

    Update MyTurtle.py

--- MyTurtle.py
+++ MyTurtle.py
@@ -1,1 +1,20 @@
 from turtle import *
+
+forward(100)
+shape('turtle')
+right(45)
+forward(150)
+
+for i in range(2):
+  print('hello')
+    
+for i in range(4):
+  forward(100)
+  right(90)
+ 
+def square():
+  for i in range(4): 
+    forward(100)
+    right(90)
+    
+square()
